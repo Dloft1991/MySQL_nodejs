6539e7ace6bb023641edbd8bb9e2c9ef39204f90
Dloft <derek.lofton1@gmail.com>
2019-02-21 12:40:00 -0500

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,7 +19,7 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   displayItems();
-  startQuestions();
+  
   
 });
 
@@ -31,11 +31,13 @@ function displayItems() {
     console.log("-------");
     console.log("Item #: " + res[0].item_id + "     Product name: " +  res[0].product_name + "     Price:$ " + res[0].price);
     console.log("-------");
+    startQuestions();
     connection.end();
+
   });
   
 }
-
+/// use of activity 14
 function startQuestions() {
   inquirer
   .prompt([
@@ -44,12 +46,47 @@ function startQuestions() {
       name: "action",
       type: "input",
       message: "What item would you like to buy? (select using item #)"
-    },
-    { 
-    name: "action",
-    type: "input",
-    message: "How many units would you like?"
     }
+  ]).then(function(answer) {
+    switch (answer.action) {
+    case "1":
+      buyiPhone();
+      break;
+
+    case "2":
+      multiSearch();
+      break;
+
+    case "3":
+      rangeSearch();
+      break;
+
+    case "4":
+      songSearch();
+      break;
+
+    case "5":
+      songAndAlbumSearch();
+      break;
+    }
+  });
   
-  ]);
+}
+
+function buyiPhone() {
+  inquirer
+  .prompt([
+    {
+      name: "action",
+      type: "input",
+      message: "How many units would you like?"
+    }
+  ])
+  .then(function(updateQuantity) {
+    connection.query("SELECT stock_quantity FROM products", function(err, res) {
+      if (err) throw err;
+      console.log("Quantity: " + res[0].stock_quantity);
+      updateQuantity();
+    });
+  });
 }
