863d3217429bf9ad59909a3daf0703ece9240afd
Dloft <derek.lofton1@gmail.com>
2019-02-21 10:33:27 -0500

    js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -17,6 +18,36 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  displayItems();
+  startQuestions();
+  
 });
 
+function displayItems() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log("-------");
+    console.log("Items for sale");
+    console.log("-------");
+    console.log("Item #: " + res[0].item_id + "     Product name: " +  res[0].product_name + "     Price:$ " + res[0].price);
+    console.log("-------");
+    connection.end();
+  });
+  
+}
+
+function startQuestions() {
+  inquirer
+  .prompt[
+      {
+      name: "action",
+      type: "input",
+      message: "What item would you like to buy? (Select using item #)"
+    },
+    {
+      name: "action",
+      type: "input",
+      message: "How many units would you like?"
+    }
+  ];
+}
