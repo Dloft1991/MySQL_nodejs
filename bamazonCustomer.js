435f36332f1efd796ee337b302e495ff6cf22013
Dloft <derek.lofton1@gmail.com>
2019-02-21 10:45:38 -0500

    working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -38,16 +38,18 @@ function displayItems() {
 
 function startQuestions() {
   inquirer
-  .prompt[
-      {
-      name: "action",
-      type: "input",
-      message: "What item would you like to buy? (Select using item #)"
-    },
+  .prompt([
     {
+      type: "text",
       name: "action",
       type: "input",
-      message: "How many units would you like?"
+      message: "What item would you like to buy? (select using item #)"
+    },
+    { 
+    name: "action",
+    type: "input",
+    message: "How many units would you like?"
     }
-  ];
+  
+  ]);
 }
